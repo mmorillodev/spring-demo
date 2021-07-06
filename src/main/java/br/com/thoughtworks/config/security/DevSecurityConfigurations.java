1c37045a87657693c89e3032a26596478d47fe5d
lamatheus.g <lamatheus.g@Matheuss-MacBook-Pro.local>
2021-07-06 15:40:34 -0300

    add profile validation to DELETE endpoint; split functionalities for development and production environment

---
+++ src/main/java/br/com/thoughtworks/config/security/DevSecurityConfigurations.java
@@ -0,0 +1,22 @@
+package br.com.thoughtworks.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile("development")
+public class DevSecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    // Authorization Configuration
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .and().csrf().disable();
+    }
+}
--- src/main/java/br/com/thoughtworks/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/thoughtworks/config/security/SecurityConfigurations.java
@@ -1,10 +1,10 @@
 package br.com.thoughtworks.config.security;
 
 import br.com.thoughtworks.controller.respository.UserRepository;
-import jdk.nashorn.internal.parser.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,8 +18,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @EnableWebSecurity
 @Configuration
+@Profile("production")
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationService authenticationService;
     @Autowired
     private TokenService tokenService;
     @Autowired
@@ -30,9 +33,6 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
-
-    @Autowired
-    private AuthenticationService authenticationService;
 
     // Authentication Configuration
     @Override
@@ -53,6 +53,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERATOR")
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
